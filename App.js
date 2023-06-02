13ffb15fa85f778a4917547d8ad247b278f7a4ba
Animesh Singh <animeshsingh.mail@gmail.com>
2023-06-02 11:01:04 +0530

    made basic search structure

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View  style={{flex:1, backgroundColor:"#DAD7D7"}}>
+      <TextInput
+        placeholder='Search'
+        placeholderTextColor={"#000"}
+        
+        style={{
+          backgroundColor: "#154c79",
+          height:45,
+          fontSize: 22,
+          padding: 5,
+          paddingLeft: 10,
+         }}
+        />
+
     </View>
   );
 }
